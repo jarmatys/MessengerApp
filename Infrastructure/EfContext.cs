ebd01b26c0f5d1150db7824c13d88251a3d8e19e
jarmatys <jarekarmatys@gmail.com>
2020-03-13 17:28:03 +0100

    Dodaj pliki projektów.

---
+++ Infrastructure/EfContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Infrastructure
+{
+    public class EfContext : IdentityDbContext
+    {
+        public EfContext(DbContextOptions<EfContext> options) : base(options) { }
+
+        // zaślepka na klasę bazową
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MessengerApp.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MessengerApp
+{
+    public class Startup
+    {
+        // Pobieranie pliku konfiguracyjnego 
+        public IConfiguration Configuration { get; }
+        public Startup()
+        {
+            var config = new ConfigurationBuilder();
+
+            config.AddJsonFile("appsettings.json");
+            config.AddEnvironmentVariables("APP_");
+
+            Configuration = config.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            // £¹czenie z baz¹ danych
+            services.AddDbContext<EfContext>(builder =>
+            {
+                builder.UseSqlServer(Configuration["Database:ConnectionString"]);
+            });
+
+            // Wstrzykiwanie zale¿noœci o identifykacji u¿ytkowników
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                // Opcje dotycz¹ce has³a
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 2;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+            }).AddEntityFrameworkStores<EfContext>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
