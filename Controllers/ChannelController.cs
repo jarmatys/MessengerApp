74fc1101d3ca49e379322ee405885885fdccd19c
jarmatys <jarekarmatys@gmail.com>
2020-03-14 14:13:19 +0100

    dodawanie kanałów

--- Controllers/ChannelController.cs
+++ Controllers/ChannelController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MessengerApp.Infrastructure;
+using MessengerApp.Models.Account;
+using MessengerApp.Models.Channel;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MessengerApp.Controllers
@@ -12,8 +15,11 @@ namespace MessengerApp.Controllers
     public class ChannelController : Controller
     {
         private readonly ChannelManager _channel;
-        public ChannelController(ChannelManager channel)
+        private readonly UserManager<User> _userManager;
+
+        public ChannelController(ChannelManager channel, UserManager<User> userManager)
         {
+            _userManager = userManager;
             _channel = channel;
         }
 
@@ -29,9 +35,29 @@ namespace MessengerApp.Controllers
             return View(channel);
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ChannelViewModel result)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                var newChannel = new ChannelModel {
+                    Color = result.Color,
+                    Name = result.Name,
+                    OwnerUser = user,
+                    isDefault = false
+                };
+                await _channel.CreateChannel(newChannel);
+                return RedirectToAction("List", "Channel");
+            }
+            return View(result);
         }
     }
 }
---
+++ Models/Channel/ChannelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Models.Channel
+{
+    public class ChannelViewModel
+    {
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
